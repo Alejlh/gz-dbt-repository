7a7fb78cee3290f2384e5bf5c7f7b50961235168
alejlh@gmail.com <alejlh@gmail.com>
2025-02-11 15:36:21 +0000

    Fin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,9 +1,13 @@
 
     select orders_id,
     date_date,
-    ROUND((margin+shipping_fee-logcost-ship_cost),2) as operational_margin,
     revenue,
     quantity,
+    purchase_cost,
+    shipping_fee,
+    logcost,
+    ship_cost,
+    ROUND((margin+shipping_fee-logcost-ship_cost),2) as operational_margin,
     from {{ ref("int_orders_margin") }} as t_s
     left join {{ ref("stg_raw__ship") }} as t_p 
    using (orders_id)
