426f3ad04aa9225f12f02066c2cb207cc081bdad
alejlh@gmail.com <alejlh@gmail.com>
2025-02-12 14:46:19 +0000

    Final

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,8 +1,8 @@
  SELECT 
  date_date, 
- ROUND(SUM(ads_cost),2) as ad_cost,
+ ROUND(SUM(ads_cost),2) as ads_cost,
  ROUND(SUM(impression),2) as ads_impressions,
- ROUND(SUM(click),2) as nb_clicks,
+ ROUND(SUM(click),2) as ads_clicks,
  FROM {{ref('int_campaigns')}}
  GROUP BY date_date
  ORDER BY date_date DESC
--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,6 +1,6 @@
 SELECT 
     date_date,
-    ROUND(operational_margin-ad_cost) AS ads_margin,
+    ROUND(operational_margin-ads_cost) AS ads_margin,
     number_transactions,
     revenue,
     average_basket,
@@ -8,6 +8,9 @@ SELECT
     purchase_cost,
     shipping_fee,
     log_cost,
+    ads_cost,
+    ads_impressions,
+    ads_clicks,
 FROM {{ ref("finance_days") }}
 LEFT JOIN {{ ref("int_campaigns_day") }}
 USING(date_date)
